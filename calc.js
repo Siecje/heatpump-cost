9b56851e06feab74e148306ab810802cd7f99d7c
Cody Scott <cody.j.b.scott@gmail.com>
2022-03-25 21:50:34 -0400

    Add Current heating method select

--- calc.js
+++ calc.js
@@ -10,6 +10,54 @@ const kWhPerUnit = {
 }
 
 
+function hideAllChildren(element){
+  for (let i = 0; i < element.children.length; i++) {
+    element.children[i].style.display = "none";
+  }
+}
+
+
+function setExisting(){
+  let existingHeat = document.getElementById("existing").value;
+  let unitSelect = document.getElementById("unit");
+
+  hideAllChildren(unitSelect);
+  if (existingHeat === "0"){
+    let emptyOption = document.getElementById("emptyOption");
+    emptyOption.style.display = "block";
+
+    let m3 = document.getElementById("m3");
+    m3.style.display = "block";
+
+    let therm = document.getElementById("therm");
+    therm.style.display = "block";
+
+    let existingPrice = document.getElementById("existingPrice");
+    existingPrice.style.display = "flex";
+
+    let existingFurnace = document.getElementById("existingFurnace");
+    existingFurnace.style.display = "block";
+  }
+  else if (existingHeat === "1" || existingHeat === "2"){
+    let litre = document.getElementById("L");
+    litre.style.display = "block";
+
+    let existingPrice = document.getElementById("existingPrice");
+    existingPrice.style.display = "flex";
+
+    let existingFurnace = document.getElementById("existingFurnace");
+    existingFurnace.style.display = "block";
+  }
+  else if (existingHeat === "3"){
+    let existingPrice = document.getElementById("existingPrice");
+    existingPrice.style.display = "none";
+
+    let existingFurnace = document.getElementById("existingFurnace");
+    existingFurnace.style.display = "none";
+  }
+}
+
+
 function calculateCOP(){
   let price = Number(document.getElementById("price").value);
   let unit = document.getElementById("unit").value;
